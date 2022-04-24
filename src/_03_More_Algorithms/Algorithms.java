9f8bc18819f07c3231b593fd27d7b5f49e40a573
league <league@imac-22.attlocal.net>
2022-04-24 16:28:40 -0700

    poiuytrewq

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -13,12 +13,57 @@ public class Algorithms {
     }
     
     public static boolean isPrime(int num1) {
-  //  for(int i = 1;i<10000;i++) {
+ for(int i =1;i<1000;i+=2) {
+	 for(int j = 3;j<1000;j+=2) {
+		 if(num1 ==i) {
+			 if(num1!=j) {
+				 if(num1%j ==0){
+					 return false;
+				 }else {
+					 if(j==999) {
+						 return true;
+					 }
+				 }
+			 }
+		 }
+	 }
+}
     	
 return false;
     }
     
+    public static boolean isSquare(int num1) {
+    	if(num1 ==1) {
+    		return true;
+    	}
+    	for(int i = 1;i<num1;i++) {
+    		if(num1/i ==i) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    
+    public static boolean isCube(double num1) { 
+    	if(num1 == 1) {
+    		return true;
+    	}
+    	for(double i = 1;i<num1;i++) {
+    		System.out.println(i);
+        	double divide1 = num1/i; 
+        	if(divide1/i==i){
+    			return true;
+    		} 
+    	}
+		return false;
+    }
+    
+    
     public static int findBrokenEgg(List<String> eggs) {
+    	for(int i = 0;i<eggs.size();i++) {
+    		
+    	}
         return 0;
     }
 }
